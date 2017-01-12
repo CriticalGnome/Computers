80259d2eac8a8f2becbe9cd01da59a6446e9304e
Сергей Калиновский <lord.skiminok@gmail.com>
2017-01-12 16:21:21 +0300

    Main method

---
+++ src/com/criticalgnome/computers/ComputersRunner.java
@@ -0,0 +1,13 @@
+package com.criticalgnome.computers;
+
+public class ComputersRunner {
+	/**
+	 * Main method. Entry point.
+	 * 
+	 * @param args
+	 *            String[]:Command line arguments
+	 */
+	public static void main(String[] args) {
+
+	}
+}
