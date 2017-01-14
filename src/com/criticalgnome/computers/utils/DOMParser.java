e90f6ce9d99ab2bdd8b93360f2409abcc2d2fdaa
Сергей Калиновский <lord.skiminok@gmail.com>
2017-01-14 17:59:14 +0300

    Abstract class created

--- src/com/criticalgnome/computers/utils/DOMParser.java
+++ src/com/criticalgnome/computers/utils/DOMParser.java
@@ -10,12 +10,16 @@ import org.w3c.dom.NodeList;
  * @author CriticalGnome
  *
  */
-public class DOMParser {
-	public static void parse() throws Exception {
+public class DOMParser extends Parser {
+
+	@Override
+	public void parse() throws Exception {
 		DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = fact.newDocumentBuilder();
 		Document doc = builder.parse("bin/com/criticalgnome/computers/xml/Device.xml");
 		NodeList list = doc.getElementsByTagName("device");
 		System.out.println(list.item(0));
+		super.parse();
 	}
+
 }
---
+++ src/com/criticalgnome/computers/utils/Parser.java
@@ -0,0 +1,11 @@
+package com.criticalgnome.computers.utils;
+
+/**
+ * @author CriticalGnome
+ *
+ */
+public abstract class Parser {
+	public void parse() throws Exception {
+
+	}
+}
