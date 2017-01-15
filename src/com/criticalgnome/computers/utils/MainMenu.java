a4e9744c3291c72b8b4152dc0d22df8a9e89f1b6
Сергей Калиновский <lord.skiminok@gmail.com>
2017-01-15 20:59:07 +0300

    Programmer's names added to main menu

--- src/com/criticalgnome/computers/utils/MainMenu.java
+++ src/com/criticalgnome/computers/utils/MainMenu.java
@@ -12,8 +12,8 @@ public class MainMenu {
 	public static void printMainMenu() {
 		System.out.println(Constants.THIСK_LINE);
 		System.out.println("1. DOM-парсер (Сергей Калиновский)");
-		System.out.println("2. SAX-парсер ()");
-		System.out.println("3. StAX-парсер ()");
+		System.out.println("2. SAX-парсер (Влад Сытый)");
+		System.out.println("3. StAX-парсер (Константин Климов)");
 		System.out.println("0. Выход");
 		System.out.println(Constants.THIN_LINE);
 	}
