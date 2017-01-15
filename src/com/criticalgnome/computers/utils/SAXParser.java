9212c7b6e26e965ff4e09bb4a6c5d478f78c032b
Admin <Admin@Admin-PC>
2017-01-15 20:27:03 +0300

    SAX

--- src/com/criticalgnome/computers/utils/SAXParser.java
+++ src/com/criticalgnome/computers/utils/SAXParser.java
@@ -1,6 +1,13 @@
 package com.criticalgnome.computers.utils;
 
 import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
 
 import com.criticalgnome.computers.beans.Device;
 
@@ -8,7 +15,18 @@ public class SAXParser extends Parser {
 	@Override
 	public Device parse(File inputFile) throws Exception {
 		super.parse(inputFile);
-		System.out.print("Not realized: ");
-		return null;
-	}
+		SAXParserFactory parcerFactory = SAXParserFactory.newInstance();
+		Handler handler = new Handler();
+		try {
+
+			SAXParser saxparce = parcerFactory.newSAXParser();
+			saxparce.parse(inputFile, handler);
+		} catch (ParserConfigurationException | SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null; // Нужно возвращать Device Проверю push
+		}
 }
